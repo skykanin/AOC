9c50cd154fad3419f233815bfd6d8714d0e6e299
skykanin <3789764+skykanin@users.noreply.github.com>
2024-12-02 17:03:28 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -1,50 +1,53 @@
 {
-  description = "Dev environment template for AOC2022";
+  description = "Dev environment template for Advent of Code!";
 
   inputs = {
-    # Unofficial library of utilities for managing Nix Flakes.
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     # Nix package set
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachSystem
-    (with flake-utils.lib.system; [ x86_64-linux x86_64-darwin aarch64-darwin ])
-    (system: {
-      # A Haskell development environment with provided tooling
-      devShells.default =
-        let
-          # The compiler version to use for development
-          compiler-version = "ghc94";
-          pkgs = nixpkgs.legacyPackages.${system};
-          inherit (pkgs) lib;
-          hpkgs = pkgs.haskell.packages.${compiler-version};
-          # HLS
-          # Haskell packages to include
-          packages = p: with p; [ megaparsec optics-core pretty-simple ];
-          # Haskell and shell tooling
-          tools = [
-            pkgs.cbqn
-            (hpkgs.ghcWithPackages packages)
-            hpkgs.ghcid
-            hpkgs.fourmolu
-            # Always put HLS in the bottom of the list
-            # so it doesn't override other exported packages
-            # like formatters and ghc...
-            hpkgs.haskell-language-server
-          ];
-          # System libraries that need to be symlinked
-          libraries = [ ];
-          libraryPath = "${lib.makeLibraryPath libraries}";
-        in pkgs.mkShell {
-          name = "dev-shell";
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
 
-          buildInputs = tools ++ libraries;
+      perSystem = { pkgs, lib, ... }: {
+        # A devShell with all kinds of languages I've used for
+        # solving Advent of Code throughout the years
+        devShells.default =
+          let
+            # The compiler version to use for development
+            compiler-version = "ghc96";
+            hpkgs = pkgs.haskell.packages.${compiler-version};
+            # HLS
+            # Haskell packages to include
+            packages = p: with p; [ megaparsec optics-core pretty-simple ];
+            # Haskell and shell tooling
+            tools = [
+              pkgs.cbqn
+              pkgs.uiua
+              (hpkgs.ghcWithPackages packages)
+              hpkgs.ghcid
+              hpkgs.fourmolu
+              # Always put HLS in the bottom of the list
+              # so it doesn't override other exported packages
+              # like formatters and ghc...
+              hpkgs.haskell-language-server
+            ];
+            # System libraries that need to be symlinked
+            libraries = [ ];
+            libraryPath = "${lib.makeLibraryPath libraries}";
+          in pkgs.mkShell {
+            name = "AOC";
 
-          LD_LIBRARY_PATH = libraryPath;
-          LIBRARY_PATH = libraryPath;
-        };
-    });
+            buildInputs = tools ++ libraries;
+
+            LD_LIBRARY_PATH = libraryPath;
+            LIBRARY_PATH = libraryPath;
+          };
+
+        formatter = pkgs.alejandra;
+    };
+  };
 }
